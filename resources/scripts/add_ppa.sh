d6b87de4cc6f3d28a643b54a10c34029cea75371
Tomas Baca <Tomas Baca>
2025-03-10 16:08:54 +0100

    + resources

---
+++ resources/scripts/add_ppa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+echo "$0: Adding MRS ROS2 Unstable PPA repository"
+
+sudo apt-get -y install curl gpg dpkg-dev
+
+ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
+
+curl -s --compressed "https://ctu-mrs.github.io/ppa-2unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-apt.list"
+sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-unstable-preferences "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-ppa-preferences.txt"
+sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-$ARCH.list"
+sudo apt-get -y update
+
+rosdep update
+
+echo "$0: Finished adding MRS ROS2 Unstable PPA repository"
