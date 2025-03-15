db397f4448c6f800059ba697679a72e28641b95f
Tomas Baca <Tomas Baca>
2025-03-15 18:47:45 +0100

    updated ros ppa adder

--- resources/scripts/add_ros_ppa.sh
+++ resources/scripts/add_ros_ppa.sh
@@ -14,10 +14,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt-get -y install ros-dev-tools
-
 sudo apt-get -y update
-
-sudo apt-get -y install ros-jazzy-desktop-full
 
 echo "$0: ROS2 PPA added"
