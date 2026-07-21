31338ac220bd82d36661cd31fefb098d1c3c95cc
Filip <4060824+filipopo@users.noreply.github.com>
2026-07-21 15:34:44 +0200

    Simplify add_ros_ppa.sh
    Removed unnecessary update and curl installation commands.

--- resources/scripts/add_ros_ppa.sh
+++ resources/scripts/add_ros_ppa.sh
@@ -7,9 +7,6 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: Adding ROS2 PPA"
 
-sudo apt-get -y update
-sudo apt-get -y install curl
-
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
